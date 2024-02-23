53b56de6f8fcf37aaf90287fae23d20afc894a0f
enogawa <111509163+enogawa@users.noreply.github.com>
2024-02-23 17:50:15 +0900

    almost

--- src/requirements/wordpress/docker-entrypoint.sh
+++ src/requirements/wordpress/docker-entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 cd /var/www/html
-
 wp config create --dbname="wordpress_db" --dbuser="user" --dbpass="password" --dbhost="mariadb" --path=/var/www/html --allow-root
-
-wp core install --url="enogawa.42.fr" --title="My-page" --admin_user="user" --admin_email="info@wp-cli.org" --allow-root --admin_password="password" --path=/var/www/html
+wp core install --url=enogawa.42.fr --title="My-page" --admin_user=user --admin_email="info@wp-cli.org" --admin_password="password" --path=/var/www/html --allow-root
+wp user create enogawa test@gmail.com --allow-root --role=author
 
 exec "$@"
