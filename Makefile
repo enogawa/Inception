b410a51c482c8053e99bc66bf3b1b25ea7564e9e
enogawa <111509163+enogawa@users.noreply.github.com>
2024-02-29 19:23:40 +0900

    volume

---
+++ Makefile
@@ -0,0 +1,34 @@
+up:
+	docker compose -f src/docker-compose.yml up --build -d
+
+build:
+	docker compose -f src/docker-compose.yml build
+
+stop:
+	docker compose -f src/docker-compose.yml stop
+
+clean:
+	docker compose -f src/docker-compose.yml down --rmi all -v
+
+restart:
+	docker compose -f src/docker-compose.yml down --rmi all -v && docker compose -f src/docker-compose.yml up --build -d
+
+nginx:
+	docker compose -f src/docker-compose.yml exec nginx bash
+
+mariadb:
+	docker compose -f src/docker-compose.yml exec mariadb bash
+
+wordpress:
+	docker compose -f src/docker-compose.yml exec wordpress bash
+
+prune-images:
+	docker image prune -fa
+
+prune:
+	docker system prune -f
+
+logs:
+	docker compose -f src/docker-compose.yml logs
+
+.PHONY: all build stop clean exec-nginx ips prune-images prune logs
--- src/requirements/mariadb/docker-entrypoint.sh
+++ src/requirements/mariadb/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-sed -i 's/^bind-address/#bind-address/' /etc/mysql/mariadb.conf.d/50-server.cnf
 service mariadb start
-mysql -u root -ppassword -e "CREATE DATABASE IF NOT EXISTS $WORDPRESS_DB_NAME;"
-mysql -u root -ppassword -e "CREATE USER IF NOT EXISTS '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD';"
-mysql -u root -ppassword -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$WORDPRESS_DB_USER'@'%';"
-mysql -u root -ppassword -e "FLUSH PRIVILEGES;"
+mysql -u root -p$WORDPRESS_DB_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $WORDPRESS_DB_NAME;"
+mysql -u root -p$WORDPRESS_DB_PASSWORD -e "CREATE USER IF NOT EXISTS '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$WORDPRESS_DB_$WORDPRESS_DB_PASSWORD';"
+mysql -u root -p$WORDPRESS_DB_PASSWORD -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$WORDPRESS_DB_USER'@'%';"
+mysql -u root -p$WORDPRESS_DB_PASSWORD -e "FLUSH PRIVILEGES;"
 service mariadb stop
+sed -i 's/^bind-address/#bind-address/' /etc/mysql/mariadb.conf.d/50-server.cnf
 exec "$@"
 
