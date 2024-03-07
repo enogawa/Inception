5a387821f171c6aeb4f1386e12b480f5f8f7e239
Eiki Nogawa <enogawa@c1r14s8.42tokyo.jp>
2024-03-07 18:23:27 +0900

    ls

--- src/requirements/wordpress/docker-entrypoint.sh
+++ src/requirements/wordpress/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 cd /var/www/html
 wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
 wp core install --url=$DOMAIN_NAME --title="My-page" --admin_user=$ADMIN_USER --admin_email=$ADMIN_EMAIL --admin_password=$ADMIN_PASS --path=$DB_PATH --allow-root
-wp user create $NEW_USER $NEW_EMAIL --allow-root --role=$ROLE
+wp user create $NEW_USER $NEW_EMAIL --allow-root --role=$ROLE --user_pass=$NEW_PASSWORD
 
 exec "$@"
